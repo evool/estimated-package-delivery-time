fa7fb1561d768b79145e1fba88d5adfa0cf370ca
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2021-05-28 18:04:12 +0200

    Added SecurityConfig class

--- src/main/java/com/metapack/estimatedpackagedeliverytime/authorization/SecurityConfig.java
+++ src/main/java/com/metapack/estimatedpackagedeliverytime/authorization/SecurityConfig.java
@@ -1,62 +1,27 @@
 package com.metapack.estimatedpackagedeliverytime.authorization;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.sql.DataSource;
-
-@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final DataSource dataSource;
-    private final ObjectMapper mapper;
-    private final RestAuthenticationFailureHandler failureHandler;
-    private final RestAuthenticationSuccessHandler successHandler;
-
-    public SecurityConfig(ObjectMapper mapper, DataSource dataSource,
-                          RestAuthenticationSuccessHandler successHandler,
-                          RestAuthenticationFailureHandler failureHandler) {
-        this.dataSource = dataSource;
-        this.mapper = mapper;
-        this.successHandler = successHandler;
-        this.failureHandler = failureHandler;
-    }
+    @Autowired
+    private MyUserDetails myUserDetails;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("{noop}password")
-                .roles("USER");
+        auth.userDetailsService(myUserDetails);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests()
-                .antMatchers("/h2-console/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilter(authenticationFilter())
-                .exceptionHandling()
-                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return NoOpPasswordEncoder.getInstance();
     }
-
-    public JsonObjectAuthenticationFilter authenticationFilter() throws Exception {
-        JsonObjectAuthenticationFilter objectAuthenticationFilter = new JsonObjectAuthenticationFilter(mapper);
-        authenticationFilter().setAuthenticationSuccessHandler(successHandler);
-        authenticationFilter().setAuthenticationFailureHandler(failureHandler);
-        authenticationFilter().setAuthenticationManager(super.authenticationManager());
-        return objectAuthenticationFilter;
-    }
-
-
 }
